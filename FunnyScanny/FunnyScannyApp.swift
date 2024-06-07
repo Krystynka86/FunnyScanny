fbf0e27cbffde27e5ecade65d6330e315c15720b
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2024-06-07 22:23:35 +0200

    Initial Commit

---
+++ FunnyScanny/FunnyScannyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyScannyApp.swift
+//  FunnyScanny
+//
+//  Created by Cristina on 2024-06-07.
+//
+
+import SwiftUI
+
+@main
+struct FunnyScannyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
